7e454a4c7ce3b2f020df46c53b71301a30501085
BoyLeeTW <boy0726@gmail.com>
2017-07-26 21:31:11 +0800

    add login, registration and forget password function

---
+++ ShareOurCost/LoginViewController.swift
@@ -0,0 +1,172 @@
+//
+//  ViewController.swift
+//  ShareOurCost
+//
+//  Created by Brad on 25/07/2017.
+//  Copyright © 2017 Brad. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var signInOrRegisterSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var signInOrUpButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var forgetPasswordButton: UIButton!
+
+    var ref: DatabaseReference!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        fullNameTextField.isHidden = true
+
+        signInOrRegisterSegmentedControl.addTarget(self, action: #selector(handleSignInOrRegisterChange), for: UIControlEvents.valueChanged)
+
+        ref = Database.database().reference()
+
+        signInOrUpButton.addTarget(self, action: #selector(handleSignInOrRegister), for: .touchUpInside)
+
+        forgetPasswordButton.addTarget(self, action: #selector(handleForgetPassword), for: .touchUpInside)
+
+    }
+
+    func handleSignInOrRegisterChange() {
+
+        if signInOrRegisterSegmentedControl.selectedSegmentIndex == 1 {
+
+            signInOrUpButton.setTitle("Register", for: .normal)
+
+            fullNameTextField.isHidden = false
+
+            forgetPasswordButton.isHidden = true
+
+        } else {
+
+            signInOrUpButton.setTitle("Sign In", for: .normal)
+
+            fullNameTextField.isHidden = true
+
+            forgetPasswordButton.isHidden = false
+
+        }
+
+    }
+
+    func handleSignInOrRegister() {
+
+        if signInOrRegisterSegmentedControl.selectedSegmentIndex == 0 {
+
+            handleSignIn()
+
+        } else {
+
+            handleRegistration()
+
+        }
+
+    }
+
+    func handleSignIn() {
+
+        if emailTextField.text == "" {
+
+            let alertController = UIAlertController(title: "Error", message: "Please enter email account", preferredStyle: .alert)
+
+            //what is handler
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+
+            alertController.addAction(defaultAction)
+
+            present(alertController, animated: true, completion: nil)
+
+        } else {
+
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+
+                if error == nil {
+
+                    print("Successful logged in!")
+
+                    self.performSegue(withIdentifier: "MatchingVC", sender: self)
+
+                    // MARK: save uid
+
+                } else {
+
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
+    func handleRegistration() {
+
+        if emailTextField.text == "" {
+
+            let alertController = UIAlertController(title: "Error", message: "Enter an email address!", preferredStyle: .alert)
+
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+
+            alertController.addAction(defaultAction)
+
+            self.present(alertController, animated: true, completion:  nil)
+
+        } else {
+
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+
+                if error == nil {
+                    print("Successful signed up!")
+
+                    self.ref?.child("Users").child("\(user!.uid)").setValue(
+                        ["email": "\(self.emailTextField.text!)",
+                            "fullName": "\(self.fullNameTextField.text!)"
+                        ])
+
+                    self.emailTextField.text = ""
+                    self.passwordTextField.text = ""
+                    self.fullNameTextField.text = ""
+
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "MatchingVC")
+                    self.present(vc!, animated: true, completion: nil)
+
+                } else {
+
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+
+                    alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
+    func handleForgetPassword() {
+
+        Auth.auth().sendPasswordReset(withEmail: emailTextField.text!) { ( error ) in
+
+            if let error = error {
+
+                print(error.localizedDescription)
+
+            } else {
+
+                print("Sent password reset mail successfully!")
+
+            }
+
+        }
+
+    }
+
+}
